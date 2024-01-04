328e56e97794581c77c921ae64bbdef0f168f275
receiptrangers <max.hart.440@gmail.com>
2024-01-04 13:13:40 +1100

    added package.json

---
+++ public/script.js
@@ -0,0 +1,9 @@
+document.getElementById('imageBtn').addEventListener('click', function() {
+    document.getElementById('content').innerHTML = '<img src="path_to_your_image.jpg" alt="receipt">';
+});
+
+document.getElementById('dataBtn').addEventListener('click', function() {
+    document.getElementById('content').innerHTML = '<p>data</p>';
+});
+
+
